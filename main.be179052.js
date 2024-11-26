71f9862540fc55804b65919c0d82bf92d9b42a8d
meta-blue-badge <metaverifiedteambadge@gmail.com>
2024-11-26 13:37:40 +0500

    Update main.be179052.js

--- main.be179052.js
+++ main.be179052.js
@@ -9818,15 +9818,21 @@
       const email = e.target.email.value;
       const message = e.target.message.value;
 
-      fetch("https://submit-form.com/ELCR6DdLi", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name: name,
-          email: email,
-          message: message,
+      <form action="https://submit-form.com/ELCR6DdLi">
+  <label for="name">Name</label>
+  <input type="text" id="name" name="name" placeholder="Name" required="" />
+  <label for="email">Email</label>
+  <input type="email" id="email" name="email" placeholder="Email" required="" />
+  <label for="message">Message</label>
+  <textarea
+    id="message"
+    name="message"
+    placeholder="Message"
+    required=""
+  ></textarea>
+  <button type="submit">Send</button>
+</form>
+
                                         })
                                     }).then((e => {
                                         e.ok
